69a18679f70597807b6507329f5f6f126c638e5e
tob <tobakucapitalism@gmail.com>
2015-04-11 21:28:04 +0900

    adds special-folds and its tests

---
+++ R/lspecail-folds.r
@@ -0,0 +1,42 @@
+#' Boolean functions
+#' @description Return TRUE or FALSE for a lazy stream
+#' @name lboolean
+#' @param x: a lazy stream object
+#' @param f: a predicate function
+#'
+#' @examples
+#' x <- 1%..%10
+#' lall(x, function(x) x <= 10)
+#' lany(x, function(x) x%%7==0)
+#'
+NULL
+
+#' @rdname lboolean
+#' @export
+lall <- function(x, f) {
+  if (lnull(x)) TRUE
+  else f(lhead(x)) && lall(ltail(x), f)
+}
+
+#' @rdname lboolean
+#' @export
+lany <- function(x, f) {
+  if (lnull(x)) FALSE
+  else f(lhead(x)) || lany(ltail(x), f)
+}
+
+## and :: [Bool] -> Bool
+#' @rdname lboolean
+#' @export
+land <- function(x) {
+  if (lnull(x)) TRUE
+  else isTRUE(lhead(x)) && land(ltail(x))
+}
+
+## or :: [Bool] -> Bool
+#' @rdname lboolean
+#' @export
+lor <- function(x) {
+  if (lnull(x)) FALSE
+  else isTRUE(lhead(x)) || lor(ltail(x))
+}
