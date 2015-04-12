9da993606c242115a28aba724d4696c76b73ca12
tob <tobakucapitalism@gmail.com>
2015-04-12 22:16:40 +0900

    change @rdname

--- R/lindexing.r
+++ R/lindexing.r
@@ -1,11 +1,15 @@
 #' Extract the nth elements (starting from 0)
 #'
+#' @name lindexing
 #' @param x a lazy stream object
 #' @param n index starting from 0
-#' @export
 #' @examples
 #' x <- llist(1, 2, 3, 4, 5)
 #' x %!!% 3 # => 4
+NULL
+
+#' @rdname lindexing
+#' @export
 `%!!%` <- function(x, n) {
   if (n < 0) stop("negative index")
   if (n == 0) lhead(x) else ltail(x) %!!% (n - 1)
