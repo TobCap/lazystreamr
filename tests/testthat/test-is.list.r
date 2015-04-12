513d067a9699dcdd38795a86fa07bcf92c81670e
tob <tobakucapitalism@gmail.com>
2015-04-12 12:55:50 +0900

    add tests for is.llist()

--- tests/testthat/test-is.list.r
+++ tests/testthat/test-is.list.r
@@ -1,10 +1,25 @@
 context("test for is.list()")
 
 test_that("test", {
+  expect_false(is.lcons(lempty))
+  expect_false(is.lpair(lempty))
   expect_true(is.llist(lempty))
+
+  expect_true(is.lcons(1 %:% 2))
+  expect_true(is.lpair(1 %:% 2))
+  expect_false(is.llist(1 %:% 2))
+
+  expect_true(is.lcons(1 %..% 2))
+  expect_false(is.lpair(1 %..% 2))
+  expect_true(is.llist(1 %..% 2))
+
   expect_true(is.llist(1 %:% (2 %:% lempty)))
   expect_true(is.llist(llist(1, 2, 3)))
   expect_true(is.llist(1 %..% 3))
+
+  expect_true(is.lcons(1 %:% (2 %:% 3)))
+  expect_true(is.lpair(1 %:% (2 %:% 3)))
+  expect_false(is.llist(1 %:% (2 %:% 3)))
 
   ones <- 1 %:% ones
   lseq_maker. <- function(..., f) ..1 %:% do.call(lseq_maker., c(list(...)[-1], do.call(f, list(...)), f = f))
